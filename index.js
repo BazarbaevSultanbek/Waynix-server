d4b30f885864c3812ecc3aec8b032898d568c0d9
Bazarbaev Sultanbek <bazarbaeoff@gmail.com>
2025-12-22 21:13:07 +0500

    access is given

--- index.js
+++ index.js
@@ -20,10 +20,17 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://waynix.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://waynix.vercel.app"
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
