821c56a7b8fa6dd3fd80c330e53516886d35062e
Bazarbaev Sultanbek <bazarbaeoff@gmail.com>
2026-02-22 16:52:13 +0500

    second try

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -2,15 +2,16 @@ const ApiError = require("../exceptions/api-error");
 
 module.exports = function (err, req, res, next) {
   if (err instanceof ApiError) {
-    if (err.status !== 401) {
-      console.log(err);
-    }
     return res.status(err.status).json({
       message: err.message,
       errors: err.errors || [],
     });
   }
 
-  console.log(err);
-  return res.status(500).json({ message: "Server error!" });
+  console.error("Unhandled error:", err);
+
+  return res.status(500).json({
+    message: err.message || "Server error!",
+    stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+  });
 };
